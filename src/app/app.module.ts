7c6728eaf20bacd5cb6ef3cc46b47d2536d38277
josh hung <lebjamesron@gmail.com>
2021-11-19 16:37:53 +0800

    added search input component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { MainComponent } from './layout/main/main.component';
+import { SearchInputComponent } from './components/search-input/search-input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MainComponent
+    MainComponent,
+    SearchInputComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/search-input/search-input.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-input',
+  template: `<input class="w-full h-full" />`,
+  styleUrls: ['./search-input.component.css'],
+})
+export class SearchInputComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
